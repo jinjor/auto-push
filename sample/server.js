07cc7ef0fd2dec3238604f2093b4f762983d8503
torii_y <torii_y@worksap.co.jp>
2015-02-27 21:51:10 +0900

    samples

--- sample/server.js
+++ sample/server.js
@@ -1,16 +1,35 @@
 var fs = require('fs');
 var autoPush = require('../index.js');
+var http = require('http');
 var http2 = require('http2');
 var ecstatic = require('ecstatic');
+var h1proxy = require('../h1proxy.js');
 
 var options = {
   key: fs.readFileSync(__dirname + '/ssl/key.pem'),
   cert: fs.readFileSync(__dirname + '/ssl/cert.pem')
 };
 
-http2.createServer(options, autoPush(ecstatic(__dirname + '/public'))).listen(8443);
+function sample0() {
+  http2.createServer(options, autoPush(ecstatic(__dirname + '/public'))).listen(8443);
+}
 
-// var h1proxy = require('../h1proxy.js');
-// http.createServer(ecstatic(__dirname + '/public')).listen(8080);
-// http2.createServer(options, autoPush(h1proxy(8080))).listen(8443);
+function sample1() {
+  http2.createServer(options, autoPush.static(__dirname + '/public')).listen(8443);
+}
 
+function sample2() {
+  autoPush.server(options, __dirname + '/public').listen(8443);
+}
+
+function sample3() {
+  http.createServer(ecstatic(__dirname + '/public')).listen(8080);
+  http2.createServer(options, autoPush(h1proxy(8080))).listen(8443);
+}
+
+function sample4() {
+  http.createServer(ecstatic(__dirname + '/public')).listen(8080);
+  http2.createServer(options, autoPush.h1LocalProxy(8080)).listen(8443);
+}
+
+[sample0, sample1, sample2, sample3, sample4][process.argv[2] || 1]();
